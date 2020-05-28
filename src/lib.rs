ce20f688bc739c9661a2ed1bd5f170a8884f5ac0
Christopher Marc Deleuze <christophermarcdeleuze@Christophers-MacBook.local>
2020-05-28 00:19:06 -0400

    Started implementing collision detection, implemented tests to see how well dijsktra would scale to small dimensions
    Dijstra's takes too long for even moderately sized properties. It could not finish for 100 meters X 100 meters (a 100 million node graph with a resolution of 1 cm).

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 mod path_finding {
+    use petgraph::algo::{bellman_ford, dijkstra};
     use petgraph::graphmap::UnGraphMap;
     pub use petgraph::Graph;
+    use std::borrow::Borrow;
+    use std::collections::HashSet;
+    use std::time::Instant;
 
     type Point = (i32, i32);
 
@@ -48,6 +52,84 @@ mod path_finding {
 
     fn _neighbors_3x3(p: Point) -> Vec<Point> {
         neighbors(p, 3, 3)
+    }
+
+    struct Line {
+        first: Point,
+        second: Point,
+    }
+
+    struct Path {
+        line: Line,
+        curr_x: i32,
+    }
+
+    impl Iterator for Path {
+        type Item = Point;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            return if self.curr_x < self.line.second.0 {
+                let line_angle_to_origin = ((self.line.second.1 - self.line.first.1) as f64
+                    / (self.line.second.0 - self.line.first.0) as f64)
+                    .atan();
+                let dx = self.curr_x - self.line.first.0;
+                let dy = dx as f64 * line_angle_to_origin.sin();
+                let y_at_this_point = self.line.first.1 + (dy as i32);
+                let next: Point = (self.curr_x, y_at_this_point);
+                self.curr_x = self.curr_x + 1;
+                Option::Some(next)
+            } else if self.curr_x == self.line.second.0 {
+                Option::Some(self.line.second)
+            } else {
+                Option::None
+            };
+        }
+    }
+
+    fn min_distance_from_line(point: Point, line: Line) -> f64 {
+        let line_angle_to_origin =
+            ((line.second.1 - line.first.1) as f64 / (line.second.0 - line.first.0) as f64).atan();
+        let point_angle_to_origin = (point.1 as f64 / point.0 as f64).atan();
+        let distance_origin_to_point = ((point.0 * point.0 + point.1 * point.1) as f64).sqrt();
+        return ((point_angle_to_origin - line_angle_to_origin).sin() * distance_origin_to_point)
+            .abs();
+    }
+
+    fn collision_between(p1: Point, p2: Point, g: UnGraphMap<Point, i32>) -> bool {
+        let path: Path = Path {
+            line: Line {
+                first: p1.clone(),
+                second: p2.clone(),
+            },
+            curr_x: p1.0,
+        };
+
+        for step in path {
+            if !g.contains_node(step) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    #[test]
+    fn find_path_dfs() {
+        // Approximately 1 cm descretization, 100 meters in length and width.
+        for graph_size_power in (7..10) {
+            let now = Instant::now();
+            let base :i32= 2;
+            let graph_size = base.pow(graph_size_power);
+            let house: petgraph::graphmap::GraphMap<(i32, i32), i32, petgraph::Undirected> =
+                build_empty_tile_graph(graph_size, graph_size);
+            dijkstra(
+                house.borrow(),
+                (0, 0),
+                Option::Some((graph_size - 1, graph_size - 1)),
+                |_| 1,
+            );
+            println!("graph size: {}, milliseconds elapsed: {}", graph_size, now.elapsed().as_millis());
+        }
     }
 
     #[test]
